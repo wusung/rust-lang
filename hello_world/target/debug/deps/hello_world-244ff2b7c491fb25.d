6003a9d97bae3c708cd8d2a061a8e772e2e53b39
wusung <twkoci@gmail.com>
2020-02-12 22:19:39 +0800

    add hello_world

---
+++ hello_world/target/debug/deps/hello_world-244ff2b7c491fb25.d
@@ -0,0 +1,5 @@
+/Users/wusungpeng/Workspace/rust-lang/hello_world/target/debug/deps/hello_world-244ff2b7c491fb25: src/main.rs
+
+/Users/wusungpeng/Workspace/rust-lang/hello_world/target/debug/deps/hello_world-244ff2b7c491fb25.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/wusungpeng/Workspace/rust-lang/hello_world/target/debug/hello_world: /Users/wusungpeng/Workspace/rust-lang/hello_world/src/main.rs
