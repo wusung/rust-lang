055d46848160d5e6ab14f4c7de41d2d0478d7545
wusung <twkoci@gmail.com>
2020-02-12 22:49:10 +0800

    add variables

---
+++ variables/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // let mut x = 3;
+    // println!("The value of x is: {}", x);
+    // x = 6;
+    // println!("The value of x is: {}", x);
+
+    let x = 5;
+
+    let x = x * 1;
+
+    let x = x * 2;
+
+    println!("The value of x is: {}", x);
+
+    let mut spaces = "   ";
+    let spaces = spaces.len();
+    println!("{}", spaces);
+}
