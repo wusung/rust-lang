f6e3c3276f56328e6394ed9469c46a25827bb3c7
wusung <twkoci@gmail.com>
2020-02-12 22:23:43 +0800

    add guessing_game

---
+++ guessing_game/target/debug/deps/guessing_game-0e489d3adc7e44d8.d
@@ -0,0 +1,5 @@
+/Users/wusungpeng/Workspace/rust-lang/guessing_game/target/debug/deps/guessing_game-0e489d3adc7e44d8: src/main.rs
+
+/Users/wusungpeng/Workspace/rust-lang/guessing_game/target/debug/deps/guessing_game-0e489d3adc7e44d8.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/wusungpeng/Workspace/rust-lang/guessing_game/target/debug/guessing_game: /Users/wusungpeng/Workspace/rust-lang/guessing_game/src/main.rs
