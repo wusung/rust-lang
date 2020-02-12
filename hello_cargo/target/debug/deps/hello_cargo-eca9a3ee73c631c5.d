3ca5fd886f3c35c6bbe1e497fd790f59d3c29a07
wusung <twkoci@gmail.com>
2020-02-12 22:20:56 +0800

    add hello_cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-eca9a3ee73c631c5.d
@@ -0,0 +1,5 @@
+/Users/wusungpeng/Workspace/rust-lang/hello_cargo/target/debug/deps/hello_cargo-eca9a3ee73c631c5.rmeta: src/main.rs
+
+/Users/wusungpeng/Workspace/rust-lang/hello_cargo/target/debug/deps/hello_cargo-eca9a3ee73c631c5.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/wusungpeng/Workspace/rust-lang/hello_cargo/target/debug/hello_cargo: /Users/wusungpeng/Workspace/rust-lang/hello_cargo/src/main.rs
